bf5f5e22fd111d0ac73b22b9c55f2226817db832
Brad Schrag <brad.schrag@gmail.com>
2019-03-26 19:01:45 -0500

    added class methods for trip and destinations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,7 +8,39 @@ class Trip < ApplicationRecord
   #default_scope { order(created_at: :desc) }
 
 #add scope method for ordering by name in the controller - define here
+
+	# playing around in rails c, to make find the correct methods
+
+	# Trip.joins(trip_destinations: :destination)
+	# to join trip and destination tables
+	def self.with_destinations
+		joins(trip_destinations: :destination)
+	end
+
+	# will group a table by common destination_ids
+	def self.group_by_destinations
+		with_destinations.group("destinations.id")
+	end
+
+	# take a grouped value
+	def self.rank_by_destinations
+		group_by_destinations.select("*, count(trips.id) as total_count").order("total_count desc")
+	end
+
+	# uses rank_by_destination, limit to top n values, ie top 5, 10, etc.
+	def self.top_n_destinations(n)
+		rank_by_destinations.limit(n)
+	end	
+
 end
 
-# scope :visible, -> {
-#   where(:visible => true)
+# now to convert these class methods into scopes, checkout this link
+
+# https://guides.rubyonrails.org/active_record_querying.html#scopes
+
+# ex:
+	# this scope would replace the .with_destinations class method
+	# scope :with_destinations, -> { joins(trip_destinations: :destination)}
+
+	# will replace group_by_destinations
+	# scope :group_by_destinations, -> { with_destinations.group("destinations.id")}
